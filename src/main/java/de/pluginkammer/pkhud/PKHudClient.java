a135a3bcf90e0c6cfa911b8be5f914799bf1ff42
tommyyt2026-ui <tommyyt2026@gmail.com>
2026-06-11 14:49:34 +0200

    Register HUD renderer and client tick event

--- src/main/java/de/pluginkammer/pkhud/PKHudClient.java
+++ src/main/java/de/pluginkammer/pkhud/PKHudClient.java
@@ -1,11 +1,20 @@
 package de.pluginkammer.pkhud;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class PKHudClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+
+        HudRenderCallback.EVENT.register(new HudRenderer());
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            PlaytimeManager.tick();
+        });
+
         System.out.println("Pluginkammer HUD geladen!");
     }
 }
